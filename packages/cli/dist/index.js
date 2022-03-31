28f0f6db1dc0b31307ff7c2560bba248a50d3fe2
cameronaziz <cameronaziz@me.com>
2022-03-31 05:59:43 -0700

    cli and cleanup complete

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cli = function (args) {
+    process.stdout.write("How did this work?\n");
+};
+exports.default = cli;
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,5 @@
+const cli = (args: string[]) => {
+  process.stdout.write(`How did this work?\n`);
+};
+
+export default cli;
